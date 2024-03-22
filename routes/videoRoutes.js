791801c0dc8aa7cc085fbf781d06f35adf333eac
mimlyrics <154952462+mimlyrics@users.noreply.github.com>
2024-03-22 21:38:38 +0100

    Update videoRoutes.js

--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const multer = require('multer');
-const DIR = "./public/mimlyrics/videos";
+
+const DIR = "./public/videos";
 const path = require('path');
 const fs = require('fs');
 const rimraf = require('rimraf');
@@ -20,6 +21,20 @@ const Video = require('../models/Video');
 const asyncHandler = require("express-async-handler");
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
+        console.log(file);
+        console.log(req.params);
+        const {genre} = req.params;
+        let DIR = "./public/videos/" + genre
+        async function createFolder() {
+            try {
+                if(!fs.existsSync(DIR)) {
+                    fs.mkdirSync(DIR);
+                }
+            }catch(err) {
+                console.log(err);
+            }
+        }
+        createFolder();
         cb(null, DIR);
     },
     filename: function(req, file, cb) {
@@ -38,18 +53,21 @@ function fileFilter(req, file, cb) {
 
 const upload = multer({storage,fileFilter, limits: {fileSize: 200_000_000}});
 
-const {deleteVideo, similarVideo, putVideos, videoViews, likeVideo, getVideo, postVideo, searchVideo, getVideoById, EditVideo, downloadVideo } = require("../controllers/videoController");
+const {deleteVideo, similarVideo, putVideos, videoViews, likeVideo, 
+    getVideo, postVideo, searchVideo, getVideoById, EditVideo, downloadVideo,
+    searchVideoByCategoryAndGenre, searchVideoByGenre } = require("../controllers/videoController");
 
-router.route("/video").post(upload.single("video"),postVideo);
-router.route("/video/:category").get(getVideo);
-router.route("/video/get/:id").get(getVideoById);
-router.route("/video/edit/:id").put(upload.single("video"),EditVideo);
-router.route("/video/similar/:mediaId").get(similarVideo);
-router.route("/video/:id").put(putVideos).delete(deleteVideo);
-router.route("/video/search/:searchId").get(searchVideo);
-router.route("/video/download/:id").get(downloadVideo);
-router.route("/video/views/:mediaId").put(videoViews);
+router.route("/video/:genre/post").post(upload.single("video"),postVideo);
+router.route("/video/:category/category").get(getVideo);
+router.route("/video/:videoId/get").get(getVideoById);
+router.route("/video/:videoId/edit").put(upload.single("video"),EditVideo);
+router.route("/video/:videoId/similar").get(similarVideo);
+router.route("/video/:category/:genre/search3").get(searchVideoByCategoryAndGenre);
+router.route("/video/:genre/search2").get(searchVideoByGenre);
+router.route("/video/:videoId").put(putVideos).delete(deleteVideo);
+router.route("/video/:searchId/search").get(searchVideo);
+router.route("/video/:videoId/download").get(downloadVideo);
+router.route("/video/:videoId/views").put(videoViews);
 // like and dislike
-router.route("/video/like/:mediaId").put(likeVideo);;
-
+router.route("/video/:videoId/likes").put(likeVideo);;
 module.exports = router;
